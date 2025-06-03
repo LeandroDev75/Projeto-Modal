c6ca0646c8401709c91c5c5bdd0becd6e4565b4a
Leandro Carone Lanschi <leandrocala2@gmail.com>
2025-06-03 09:55:03 -0300

    [feat] Criação de modal

---
+++ logica.js
@@ -0,0 +1,28 @@
+
+let modal = document.querySelector(".container");
+
+function onOpen() {
+
+modal.classList.add("active");
+
+}
+
+function onClose() {
+    modal.classList.remove("active");
+}
+
+
+let modal1 = document.querySelector(".container1");
+
+function abrirModal1() {
+
+    modal1.classList.add("active1");
+
+}
+
+function fecharModal() {
+
+modal1.classList.remove("active1");
+
+}
+
